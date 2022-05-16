7e26613aa868278a078467d0801fd87378dffad5
saminahaq <saminanaseem2019@gmail.com>
2022-05-15 19:22:24 -0700

    ch03

---
+++ Chapter#03/src/Manager.java
@@ -0,0 +1,11 @@
+public class Manager {
+    private String emailAddress;
+
+    public Manager(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }    
+}
